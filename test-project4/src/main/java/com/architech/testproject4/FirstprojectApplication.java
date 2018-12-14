c5f733096a29d119950eff56ae9d13b65d863f65
Nischith Muthyala <nischithreddy92@gmail.com>
2018-12-14 11:58:23 -0500

    first commit

---
+++ test-project4/src/main/java/com/architech/testproject4/FirstprojectApplication.java
@@ -0,0 +1,14 @@
+package com.architech.testproject4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectApplication.class, args);
+	}
+
+}
+
